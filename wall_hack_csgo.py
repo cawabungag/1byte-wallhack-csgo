500efdc412f67c2b951db3b2a1e9b26257ae544f
Ilyas <38839317+cawabungag@users.noreply.github.com>
2021-03-18 14:53:52 +0300

    Update wall_hack_csgo.py

--- wall_hack_csgo.py
+++ wall_hack_csgo.py
@@ -2,11 +2,6 @@ from tkinter import *
 import pymem
 import re
 
-pm = pymem.Pymem('csgo.exe')
-client = pymem.process.module_from_name(pm.process_handle,
-                                        'client.dll')
-clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
-
 
 def wallhack_action():
     if wallhack_button["text"] == "Start Wallhack":
@@ -14,6 +9,10 @@ def wallhack_action():
     else:
         wallhack_button.configure(text="Start Wallhack")
     try:
+        pm = pymem.Pymem('csgo.exe')
+        client = pymem.process.module_from_name(pm.process_handle,
+                                                'client.dll')
+        clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
         address = client.lpBaseOfDll + re.search(rb"\x83\xF8.\x8B\x45\x08\x0F", clientModule).start() + 2
         pm.write_uchar(address, 2 if pm.read_uchar(address) == 1 else 1)
         pm.close_process()
@@ -27,6 +26,10 @@ def radar_action():
     else:
         radar_button.configure(text="Start Radar")
     try:
+        pm = pymem.Pymem('csgo.exe')
+        client = pymem.process.module_from_name(pm.process_handle,
+                                                'client.dll')
+        clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
         address = client.lpBaseOfDll + re.search(rb"\x80\xB9.{5}\x74\x12\x8B\x41\x08", clientModule).start() + 6
         pm.write_uchar(address, 0 if pm.read_uchar(address) != 0 else 2)
         pm.close_process()
