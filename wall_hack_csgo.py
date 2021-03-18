a3aa7b2bd1f95742469daa92c49a9ca350fea4e0
Ilyas <38839317+cawabungag@users.noreply.github.com>
2021-03-18 14:33:53 +0300

    Update wall_hack_csgo.py

--- wall_hack_csgo.py
+++ wall_hack_csgo.py
@@ -14,10 +14,10 @@ def wallhack_action():
                                                 'client.dll')
 
         clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
-        address = client.lpBaseOfDll + re.search(rb'\x80\xB9.{5}\x74\x12\x8B\x41\x08',
-                                                 clientModule).start() + 6
+        address = client.lpBaseOfDll + re.search(rb'\x83\xF8.\x8B\x45\x08\x0F',
+                                                 clientModule).start() + 2
 
-        pm.write_uchar(address, 0 if pm.read_uchar(address) != 0 else 2)
+        pm.write_uchar(address, 2 if pm.read_uchar(address) == 1 else 1)
         pm.close_process()
     except:
         wallhack_button.configure(text="Start Wallhack")
